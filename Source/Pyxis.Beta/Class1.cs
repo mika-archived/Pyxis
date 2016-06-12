1d1457b6b54416578092b2589f67540c3393b34e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-13 01:30:41 +0900

    Add icon and beta project.

---
+++ Source/Pyxis.Beta/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyxis.Beta
+{
+    public class Class1
+    {
+    }
+}
