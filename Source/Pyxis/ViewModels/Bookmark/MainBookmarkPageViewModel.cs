dff72bc92de206b76bf3b8a5de3dcf7ac0a655e6
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-19 22:03:15 +0900

    ブックマークページ

---
+++ Source/Pyxis/ViewModels/Bookmark/MainBookmarkPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Prism.Windows.Navigation;
+
+using Pyxis.Helpers;
+
+namespace Pyxis.ViewModels.Bookmark
+{
+    public class MainBookmarkPageViewModel : ViewModel
+    {
+        private readonly INavigationService _navigationService;
+
+        public MainBookmarkPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        #region Overrides of ViewModelBase
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            RunHelper.RunLater(RedirectToLoginPageWhenNoLogin, TimeSpan.FromMilliseconds(10));
+        }
+
+        #endregion
+
+        private void RedirectToLoginPageWhenNoLogin()
+        {
+            _navigationService.Navigate("Error.LoginRequired", null);
+        }
+    }
+}
---
+++ Source/Pyxis/Views/Bookmark/MainBookmarkPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Controls;
+
+using Pyxis.ViewModels.Bookmark;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace Pyxis.Views.Bookmark
+{
+    /// <summary>
+    ///     それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainBookmarkPage : Page
+    {
+        public MainBookmarkPageViewModel ViewModel => DataContext as MainBookmarkPageViewModel;
+
+        public MainBookmarkPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
