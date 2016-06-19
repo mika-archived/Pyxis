db4d7d7ea7149cddb4761c9b03f3c7758d1e641c
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-19 22:14:53 +0900

    フォローページ

---
+++ Source/Pyxis/ViewModels/Following/PrivateFollowingPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Windows.Navigation;
+
+namespace Pyxis.ViewModels.Following
+{
+    public class PrivateFollowingPageViewModel : ViewModel
+    {
+        public INavigationService NavigationService { get; }
+
+        public PrivateFollowingPageViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+    }
+}
---
+++ Source/Pyxis/Views/Following/PrivateFollowingPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Controls;
+
+using Pyxis.ViewModels.Following;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace Pyxis.Views.Following
+{
+    /// <summary>
+    ///     それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class PrivateFollowingPage : Page
+    {
+        public PrivateFollowingPageViewModel ViewModel => DataContext as PrivateFollowingPageViewModel;
+
+        public PrivateFollowingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
