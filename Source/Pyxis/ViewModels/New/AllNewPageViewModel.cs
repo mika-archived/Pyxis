1f4b4cf149a89067780da75aeb9405c7c0858c9b
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-23 09:17:13 +0900

    新着ページのレイアウト
    * 未ログイン (Follow, Mypixiv)
    * 画面遷移 (All)

--- Source/Pyxis/ViewModels/New/AllNewPageViewModel.cs
+++ Source/Pyxis/ViewModels/New/AllNewPageViewModel.cs
@@ -1,5 +1,8 @@
-﻿using Prism.Windows.Navigation;
+﻿using System.Collections.Generic;
 
+using Prism.Windows.Navigation;
+
+using Pyxis.Models.Parameters;
 using Pyxis.ViewModels.Base;
 
 namespace Pyxis.ViewModels.New
@@ -12,5 +15,33 @@ namespace Pyxis.ViewModels.New
         {
             NavigationService = navigationService;
         }
+
+        private void Initialize(HomeParameter parameter)
+        {
+            SubSelectdIndex = (int) parameter.ContentType;
+        }
+
+        #region Overrides of ViewModelBase
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            var parameters = ParameterBase.ToObject<HomeParameter>(e.Parameter?.ToString());
+            Initialize(parameters);
+        }
+
+        #endregion
+
+        #region SubSelectdIndex
+
+        private int _subSelectedIndex;
+
+        public int SubSelectdIndex
+        {
+            get { return _subSelectedIndex; }
+            set { SetProperty(ref _subSelectedIndex, value); }
+        }
+
+        #endregion
     }
 }
