86f1ab753f40b727cb096a381af5aa4f93bd7b84
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-29 14:08:41 +0900

    ブクマタグ

---
+++ Source/Pyxis/Models/PixivBookmarkTag.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+using Microsoft.Practices.ObjectBuilder2;
+
+using Pyxis.Beta.Interfaces.Models.v1;
+using Pyxis.Beta.Interfaces.Rest;
+using Pyxis.Models.Enums;
+
+namespace Pyxis.Models
+{
+    internal class PixivBookmarkTag : ISupportIncrementalLoading
+    {
+        private readonly IPixivClient _pixivClient;
+        private string _offset;
+        private RestrictType _restrict;
+        public ObservableCollection<IBookmarkTag> BookmarkTags { get; }
+
+        public PixivBookmarkTag(IPixivClient pixivClient)
+        {
+            _pixivClient = pixivClient;
+            BookmarkTags = new ObservableCollection<IBookmarkTag>();
+#if OFFLINE
+            HasMoreItems = false;
+#else
+            HasMoreItems = true;
+#endif
+        }
+
+        public void Query(RestrictType restrict)
+        {
+            BookmarkTags.Clear();
+            _restrict = restrict;
+            _offset = "";
+#if !OFFLINE
+            HasMoreItems = true;
+#endif
+        }
+
+        private async Task QueryAsync()
+        {
+            var tags = await _pixivClient.User.BookmarkTags.IllustAsync(restrict => _restrict.ToParamString(),
+                                                                        offset => _offset);
+            tags?.BookmarkTagList.ForEach(w => BookmarkTags.Add(w));
+            if (string.IsNullOrWhiteSpace(tags?.NextUrl))
+                HasMoreItems = false;
+            else
+                _offset = UrlParameter.ParseQuery(tags.NextUrl)["offset"];
+        }
+
+        #region Implementation of ISupportIncrementalLoading
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            return Task.Run(async () =>
+            {
+                await QueryAsync();
+                return new LoadMoreItemsResult {Count = 30};
+            }).AsAsyncOperation();
+        }
+
+        public bool HasMoreItems { get; private set; }
+
+        #endregion
+    }
+}
