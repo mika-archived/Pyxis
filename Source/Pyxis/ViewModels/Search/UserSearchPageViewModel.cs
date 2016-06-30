4fbdf349fa22cc9380acb1f1e17de6ac1d16eb00
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-06-30 11:57:09 +0900

    User search page (WIP)

---
+++ Source/Pyxis/ViewModels/Search/UserSearchPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Windows.Navigation;
+
+using Pyxis.ViewModels.Base;
+
+namespace Pyxis.ViewModels.Search
+{
+    public class UserSearchPageViewModel : ViewModel
+    {
+        public INavigationService NavigationService { get; }
+
+        public UserSearchPageViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        public void OnQuerySubmitted()
+        {
+
+        }
+
+        public void OnButtonTapped()
+        {
+
+        }
+
+        #region QueryText
+
+        private string _queryText;
+
+        public string QueryText
+        {
+            get { return _queryText; }
+            set { SetProperty(ref _queryText, value); }
+        }
+
+        #endregion
+    }
+}
