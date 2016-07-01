9ab2d1cf4de90532b6e98c87ab05254c74dbc4bb
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-07-01 10:37:57 +0900

    レイアウト

---
+++ Source/Pyxis/ViewModels/Search/Items/UserCardViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using Prism.Windows.Navigation;
+
+using Pyxis.Beta.Interfaces.Models.v1;
+using Pyxis.Models;
+using Pyxis.Mvvm;
+using Pyxis.Services.Interfaces;
+using Pyxis.ViewModels.Base;
+
+using Reactive.Bindings.Extensions;
+
+namespace Pyxis.ViewModels.Search.Items
+{
+    internal class UserCardViewModel : TappableThumbnailViewModel
+    {
+        private IImageStoreService _imageStoreService;
+        private INavigationService _navigationService;
+        private IUserPreview _userPreview;
+
+        public UserCardViewModel(IUserPreview userPreview, IImageStoreService imageStoreService,
+                                 INavigationService navigationService)
+        {
+            _userPreview = userPreview;
+            _imageStoreService = imageStoreService;
+            _navigationService = navigationService;
+
+            ThumbnailPath = PyxisConstants.DummyIcon;
+            Thumbnailable = new PixivUserImage(userPreview.User, imageStoreService);
+            Thumbnailable.ObserveProperty(w => w.ThumbnailPath)
+                         .Where(w => !string.IsNullOrWhiteSpace(w))
+                         .ObserveOnUIDispatcher()
+                         .Subscribe(w => ThumbnailPath = w)
+                         .AddTo(this);
+        }
+
+        #region Overrides of TappableThumbnailViewModel
+
+        public override void OnItemTapped()
+        {
+            base.OnItemTapped();
+        }
+
+        #endregion
+    }
+}
--- Source/Pyxis/ViewModels/Search/UserSearchPageViewModel.cs
+++ Source/Pyxis/ViewModels/Search/UserSearchPageViewModel.cs
@@ -1,16 +1,44 @@
-﻿using Prism.Windows.Navigation;
+﻿using System.Collections.Generic;
 
+using Prism.Windows.Navigation;
+
+using Pyxis.Beta.Interfaces.Models.v1;
+using Pyxis.Beta.Interfaces.Rest;
+using Pyxis.Collections;
+using Pyxis.Helpers;
+using Pyxis.Models;
+using Pyxis.Models.Enums;
+using Pyxis.Services.Interfaces;
 using Pyxis.ViewModels.Base;
+using Pyxis.ViewModels.Search.Items;
 
 namespace Pyxis.ViewModels.Search
 {
     public class UserSearchPageViewModel : ViewModel
     {
+        private readonly IAccountService _accountService;
+        private readonly IImageStoreService _imageStoreService;
+        private readonly IPixivClient _pixivClient;
+        private PixivRecommended _pixivRecommended;
         public INavigationService NavigationService { get; }
 
-        public UserSearchPageViewModel(INavigationService navigationService)
+        public IncrementalObservableCollection<TappableThumbnailViewModel> RecommendedUsers { get; }
+
+        public UserSearchPageViewModel(IAccountService accountService, IImageStoreService imageStoreService,
+                                       INavigationService navigationService,
+                                       IPixivClient pixivClient)
         {
+            _accountService = accountService;
+            _imageStoreService = imageStoreService;
+            _pixivClient = pixivClient;
             NavigationService = navigationService;
+            RecommendedUsers = new IncrementalObservableCollection<TappableThumbnailViewModel>();
+        }
+
+        private void Initialize()
+        {
+            _pixivRecommended = new PixivRecommended(_accountService, _pixivClient, ContentType.User);
+            ModelHelper.ConnectTo(RecommendedUsers, _pixivRecommended, w => w.RecommendedUsers, CreateUserViewModel);
         }
 
         public void OnQuerySubmitted()
@@ -22,6 +50,23 @@ namespace Pyxis.ViewModels.Search
         {
 
         }
+
+        #region Converters
+
+        private TappableThumbnailViewModel CreateUserViewModel(IUserPreview userPreview)
+            => new UserCardViewModel(userPreview, _imageStoreService, NavigationService);
+
+        #endregion
+
+        #region Overrides of ViewModelBase
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            Initialize();
+        }
+
+        #endregion
 
         #region QueryText
 
