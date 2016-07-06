640f4946fce60f93c91c0a7c27b131c9e81af24e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-07-06 20:12:29 +0900

    work

--- Source/Pyxis/ViewModels/WorkMainPageViewModel.cs
+++ Source/Pyxis/ViewModels/WorkMainPageViewModel.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using Prism.Windows.Navigation;
 
+using Pyxis.Beta.Interfaces.Models.v1;
 using Pyxis.Beta.Interfaces.Rest;
+using Pyxis.Collections;
+using Pyxis.Helpers;
+using Pyxis.Models;
+using Pyxis.Models.Enums;
 using Pyxis.Models.Parameters;
 using Pyxis.Services.Interfaces;
 using Pyxis.ViewModels.Base;
+using Pyxis.ViewModels.Items;
 
 namespace Pyxis.ViewModels
 {
@@ -14,8 +21,10 @@ namespace Pyxis.ViewModels
         private readonly IAccountService _accountService;
         private readonly IImageStoreService _imageStoreService;
         private readonly IPixivClient _pixivClient;
+        private PixivWork _pixivWork;
 
         public INavigationService NavigationService { get; }
+        public IncrementalObservableCollection<TappableThumbnailViewModel> WorkItems { get; }
 
         public WorkMainPageViewModel(IAccountService accountService, IImageStoreService imageStoreService,
                                      INavigationService navigationService, IPixivClient pixivClient)
@@ -24,11 +33,23 @@ namespace Pyxis.ViewModels
             _imageStoreService = imageStoreService;
             NavigationService = navigationService;
             _pixivClient = pixivClient;
+            WorkItems = new IncrementalObservableCollection<TappableThumbnailViewModel>();
         }
 
         private void Initialize(WorkParameter parameter)
         {
             SelectedIndex = (int) parameter.ContentType;
+            _pixivWork = new PixivWork(_accountService.LoggedInAccount.Id, parameter.ContentType, _pixivClient);
+            if (parameter.ContentType != ContentType.Novel)
+                ModelHelper.ConnectTo(WorkItems, _pixivWork, w => w.Illusts, CreatePixivImage);
+            else
+                ModelHelper.ConnectTo(WorkItems, _pixivWork, w => w.Novels, CreatePixivNovel);
+        }
+
+        private void RedirectoToLoginPage(WorkParameter parameter)
+        {
+            var param = new RedirectParameter {RedirectTo = "WorkMain", Parameter = parameter};
+            NavigationService.Navigate("Error.LoginRequired", param.ToJson());
         }
 
         #region Overrides of ViewModelBase
@@ -37,8 +58,21 @@ namespace Pyxis.ViewModels
         {
             base.OnNavigatedTo(e, viewModelState);
             var parameter = ParameterBase.ToObject<WorkParameter>((string) e?.Parameter);
-            Initialize(parameter);
+            if (_accountService.IsLoggedIn)
+                Initialize(parameter);
+            else
+                RunHelper.RunLater(RedirectoToLoginPage, parameter, TimeSpan.FromMilliseconds(10));
         }
+
+        #endregion
+
+        #region Converters
+
+        private PixivThumbnailViewModel CreatePixivImage(IIllust w) =>
+            new PixivThumbnailViewModel(w, _imageStoreService, NavigationService);
+
+        private PixivThumbnailViewModel CreatePixivNovel(INovel w) =>
+            new PixivThumbnailViewModel(w, _imageStoreService, NavigationService);
 
         #endregion
 
