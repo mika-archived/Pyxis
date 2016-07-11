aa51eaee09136537046e79f0885d77735ad8988d
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-07-11 16:05:02 +0900

    Mypixiv

--- Source/Pyxis/Views/MypixivMainPage.xaml.cs
+++ Source/Pyxis/Views/MypixivMainPage.xaml.cs
@@ -1,19 +1,19 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-using Pyxis.ViewModels.Mypixiv;
+using Pyxis.ViewModels;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
-namespace Pyxis.Views.Mypixiv
+namespace Pyxis.Views
 {
     /// <summary>
     ///     それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
-    public sealed partial class MainMypixivPage : Page
+    public sealed partial class MypixivMainPage : Page
     {
-        public MainMypixivPageViewModel ViewModel => DataContext as MainMypixivPageViewModel;
+        public MypixivMainPageViewModel ViewModel => DataContext as MypixivMainPageViewModel;
 
-        public MainMypixivPage()
+        public MypixivMainPage()
         {
             InitializeComponent();
         }
