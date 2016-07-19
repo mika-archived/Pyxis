c4b7e5b5e6e24af4fb898f9c3c8a06b3bce103c5
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-07-20 02:37:34 +0900

    画像 pre 読み込み

---
+++ Source/Pyxis/Behaviors/UniformToParentControlBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Xaml;
+
+using Microsoft.Xaml.Interactivity;
+
+namespace Pyxis.Behaviors
+{
+    /// <summary>
+    ///     <para>Target に指定したコントロールに対して、縦横比を維持したまま、最大サイズになるように</para>
+    ///     <para>コントロールのサイズを調節します。</para>
+    /// </summary>
+    internal class UniformToParentControlBehavior : Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty TargetProperty =
+            DependencyProperty.Register(nameof(Target), typeof(FrameworkElement), typeof(UniformToParentControlBehavior),
+                                        new PropertyMetadata(null, PropertyChangedCallback));
+
+        public FrameworkElement Target
+        {
+            get { return (FrameworkElement) GetValue(TargetProperty); }
+            set { SetValue(TargetProperty, value); }
+        }
+
+        private static void PropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var senderThis = sender as UniformToParentControlBehavior;
+            var element = sender.GetValue(TargetProperty) as FrameworkElement;
+            if (element == null || senderThis == null)
+                return;
+            element.SizeChanged += (sender1, e1) =>
+            {
+                // Width ベース変換
+                if (senderThis.AssociatedObject == null || double.IsNaN(senderThis.AssociatedObject.MaxHeight))
+                    return;
+                var target = senderThis.AssociatedObject;
+                var aspectRatio = senderThis.AssociatedObject.MaxHeight / senderThis.AssociatedObject.MaxWidth;
+                var gridSize = e1.NewSize;
+                target.Width = gridSize.Width;
+                target.Height = gridSize.Width * aspectRatio;
+            };
+        }
+    }
+}
--- Source/Pyxis/Models/PixivImage.cs
+++ Source/Pyxis/Models/PixivImage.cs
@@ -13,18 +13,31 @@ namespace Pyxis.Models
         private readonly IIllust _illust;
         private readonly IImageStoreService _imageStoreService;
         private readonly bool _isRaw;
+        private readonly bool _isShadow;
 
-        public PixivImage(IIllust illust, IImageStoreService imageStoreService, bool isRaw = false)
+        /// <summary>
+        /// </summary>
+        /// <param name="illust"></param>
+        /// <param name="imageStoreService"></param>
+        /// <param name="isRaw">オリジナルサイズを取得</param>
+        /// <param name="isShadow"><code>isRaw = true</code>の際、シャドウデータを表示</param>
+        public PixivImage(IIllust illust, IImageStoreService imageStoreService, bool isRaw = false, bool isShadow = true)
         {
             _illust = illust;
             _imageStoreService = imageStoreService;
             _isRaw = isRaw;
+            _isShadow = isShadow;
         }
 
         public override void ShowThumbnail() => RunHelper.RunAsync(DownloadImage);
 
         private async Task DownloadImage()
         {
+            if (_isRaw && _isShadow)
+            {
+                if (await _imageStoreService.ExistImageAsync(_illust.ImageUrls.SquareMedium))
+                    ThumbnailPath = await _imageStoreService.LoadImageAsync(_illust.ImageUrls.SquareMedium);
+            }
             if (!_isRaw)
             {
                 if (await _imageStoreService.ExistImageAsync(_illust.ImageUrls.SquareMedium))
