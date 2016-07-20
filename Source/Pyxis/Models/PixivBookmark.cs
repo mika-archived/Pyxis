d7fea49278df4f2c73aee493fe8105c3070c203b
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-07-21 04:23:54 +0900

    ブックマークを追加

---
+++ Source/Pyxis/Models/PixivBookmark.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+using Microsoft.Practices.ObjectBuilder2;
+
+using Pyxis.Beta.Interfaces.Models.v1;
+using Pyxis.Beta.Interfaces.Rest;
+
+namespace Pyxis.Models
+{
+    internal class PixivBookmark : ISupportIncrementalLoading
+    {
+        private readonly IPixivClient _pixivClient;
+        private string _offset;
+
+        public ObservableCollection<INovel> Novels { get; }
+
+        public PixivBookmark(IPixivClient pixivClient)
+        {
+            _pixivClient = pixivClient;
+            _offset = "";
+            Novels = new ObservableCollection<INovel>();
+#if OFFLINE
+            this.HasMoreItems = false;
+#else
+            HasMoreItems = true;
+#endif
+        }
+
+        private async Task Fetch()
+        {
+            var novels = await _pixivClient.NovelV1.MarkersAsync(offset => _offset);
+            novels?.NovelList.ForEach(w => Novels.Add(w));
+            if (string.IsNullOrWhiteSpace(novels?.NextUrl))
+                HasMoreItems = false;
+            else
+                _offset = UrlParameter.ParseQuery(novels.NextUrl)["offset"];
+        }
+
+        #region Implementation of ISupportIncrementalLoading
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            return Task.Run(async () =>
+            {
+                await Fetch();
+                return new LoadMoreItemsResult {Count = 30};
+            }).AsAsyncOperation();
+        }
+
+        public bool HasMoreItems { get; private set; }
+
+        #endregion
+    }
+}
---
+++ Source/Pyxis/ViewModels/BookmarkMainPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+using Prism.Windows.Navigation;
+
+using Pyxis.Beta.Interfaces.Models.v1;
+using Pyxis.Beta.Interfaces.Rest;
+using Pyxis.Collections;
+using Pyxis.Helpers;
+using Pyxis.Models;
+using Pyxis.Services.Interfaces;
+using Pyxis.ViewModels.Base;
+using Pyxis.ViewModels.Items;
+
+namespace Pyxis.ViewModels
+{
+    public class BookmarkMainPageViewModel : ViewModel
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IImageStoreService _imageStoreService;
+        private readonly INavigationService _navigationService;
+        private readonly IPixivClient _pixivClient;
+        private PixivBookmark _pixivBookmark;
+        public IncrementalObservableCollection<TappableThumbnailViewModel> BookmarkItems { get; }
+
+        public BookmarkMainPageViewModel(ICategoryService categoryService, IImageStoreService imageStoreService,
+                                         INavigationService navigationService, IPixivClient pixivClient)
+        {
+            _categoryService = categoryService;
+            _imageStoreService = imageStoreService;
+            _navigationService = navigationService;
+            _pixivClient = pixivClient;
+            BookmarkItems = new IncrementalObservableCollection<TappableThumbnailViewModel>();
+        }
+
+        #region Overrides of ViewModelBase
+
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+            Initialize();
+        }
+
+        #endregion
+
+        #region Initializer
+
+        private void Initialize()
+        {
+            _categoryService.UpdateCategory();
+            _pixivBookmark = new PixivBookmark(_pixivClient);
+            ModelHelper.ConnectTo(BookmarkItems, _pixivBookmark, w => w.Novels, CreatePixivNovel);
+        }
+
+        #endregion
+
+        #region Converters
+
+        private PixivThumbnailViewModel CreatePixivNovel(INovel w) =>
+            new PixivThumbnailViewModel(w, _imageStoreService, _navigationService);
+
+        #endregion
+    }
+}
