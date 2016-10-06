9998957e80a3be052aa16f8f25601e64c24d14b2
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-10-06 11:28:42 +0900

    General to Maintenance

---
+++ Source/Pyxis/ViewModels/Settings/SettingsMaintenanceViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Windows.Storage;
+
+using Microsoft.Practices.ObjectBuilder2;
+
+using Pyxis.Extensions;
+using Pyxis.Helpers;
+using Pyxis.Models.Cache;
+using Pyxis.ViewModels.Base;
+
+using WinRTXamlToolkit.IO.Extensions;
+
+namespace Pyxis.ViewModels.Settings
+{
+    public class SettingsMaintenanceViewModel : ResourceViewModel
+    {
+        public SettingsMaintenanceViewModel()
+        {
+            CacheSize = Resources.GetString("Calculating/Text");
+            FileCount = Resources.GetString("Calculating/Text");
+            RunHelper.RunLaterUI(CalcCacheSize, TimeSpan.FromMilliseconds(100));
+        }
+
+        private void CalcCacheSize()
+        {
+            using (var db = new CacheContext())
+            {
+                var count = db.CacheFiles.Count();
+                FileCount = string.Format(Resources.GetString("Items/Text"), count);
+                CacheSize = ((ulong) db.CacheFiles.Select(w => w.Size).Sum()).GetSizeString();
+                IsEnabled = count > 0;
+            }
+        }
+
+        public void ClearCache()
+        {
+            FileCount = Resources.GetString("ZeroItems/Text");
+            CacheSize = ((ulong) 0).GetSizeString();
+            IsEnabled = false;
+            Task.Run(async () =>
+            {
+                var temporaryFolder = ApplicationData.Current.TemporaryFolder;
+                var tasks = new[]
+                {
+                    await temporaryFolder.GetFolderWhenNotFoundReturnNullAsync("original"),
+                    await temporaryFolder.GetFolderWhenNotFoundReturnNullAsync("thumbnails"),
+                    await temporaryFolder.GetFolderWhenNotFoundReturnNullAsync("users"),
+                    await temporaryFolder.GetFolderWhenNotFoundReturnNullAsync("background")
+                }.Where(w => w != null).Select(async w => await w.DeleteAsync());
+                await Task.WhenAll(tasks);
+                using (var db = new CacheContext())
+                {
+                    // ReSharper disable once AccessToDisposedClosure
+                    db.CacheFiles.ForEach(w => db.CacheFiles.Remove(w));
+                    db.SaveChanges();
+                }
+            }).ContinueWith(w => CalcCacheSize());
+        }
+
+        #region FileCount
+
+        private string _fileCount;
+
+        public string FileCount
+        {
+            get { return _fileCount; }
+            set { SetProperty(ref _fileCount, value); }
+        }
+
+        #endregion
+
+        #region CacheSize
+
+        private string _cacheSize;
+
+        public string CacheSize
+        {
+            get { return _cacheSize; }
+            set { SetProperty(ref _cacheSize, value); }
+        }
+
+        #endregion
+
+        #region IsEnabled
+
+        private bool _isEnabled;
+
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set { SetProperty(ref _isEnabled, value); }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Pyxis/Views/Settings/SettingsMaintenanceView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+using Pyxis.ViewModels.Settings;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Pyxis.Views.Settings
+{
+    public sealed partial class SettingsMaintenanceView : UserControl
+    {
+        public SettingsMaintenanceViewModel ViewModel => DataContext as SettingsMaintenanceViewModel;
+
+        public SettingsMaintenanceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
