ce21f9482fb9441e87e6b6ef62e75d59b5eafeea
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-09 18:03:06 +0900

    Concurrently

--- Source/Pyxis/Services/QueryCacheService.cs
+++ Source/Pyxis/Services/QueryCacheService.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
 
+using Pyxis.Models;
 using Pyxis.Models.Caching;
 using Pyxis.Services.Interfaces;
 
@@ -12,6 +13,8 @@ namespace Pyxis.Services
 {
     public class QueryCacheService : IQueryCacheService
     {
+        private readonly AsyncLock _asyncLock = new AsyncLock();
+        private readonly object _lockObj = new object();
         private readonly List<QueryCache> _queryCaches;
 
         public QueryCacheService()
@@ -21,26 +24,32 @@ namespace Pyxis.Services
 
         public T Run<T>(Func<Expression<Func<string, object>>[], T> dispatcher, params Expression<Func<string, object>>[] query)
         {
-            var builder = new QueryBuilder(dispatcher.GetMethodInfo().Name);
-            builder.AddParams(query);
-            var cache = _queryCaches.SingleOrDefault(w => w.IsEnabled && (w.Query == builder.ToQuery()));
-            if (cache != null)
-                return (T) cache.Result;
-            var result = dispatcher.Invoke(query);
-            _queryCaches.Add(new QueryCache {Query = builder.ToQuery(), Result = result});
-            return result;
+            lock (_lockObj)
+            {
+                var builder = new QueryBuilder(dispatcher.GetMethodInfo().Name);
+                builder.AddParams(query);
+                var cache = _queryCaches.SingleOrDefault(w => w.IsEnabled && (w.Query == builder.ToQuery()));
+                if (cache != null)
+                    return (T) cache.Result;
+                var result = dispatcher.Invoke(query);
+                _queryCaches.Add(new QueryCache {Query = builder.ToQuery(), Result = result});
+                return result;
+            }
         }
 
         public async Task<T> RunAsync<T>(Func<Expression<Func<string, object>>[], Task<T>> dispatcher, params Expression<Func<string, object>>[] query)
         {
-            var builder = new QueryBuilder(dispatcher.GetMethodInfo().Name);
-            builder.AddParams(query);
-            var cache = _queryCaches.SingleOrDefault(w => w.IsEnabled && (w.Query == builder.ToQuery()));
-            if (cache != null)
-                return (T) cache.Result;
-            var result = await dispatcher.Invoke(query);
-            _queryCaches.Add(new QueryCache {Query = builder.ToQuery(), Result = result});
-            return result;
+            using (await _asyncLock.LockAsync())
+            {
+                var builder = new QueryBuilder(dispatcher.GetMethodInfo().Name);
+                builder.AddParams(query);
+                var cache = _queryCaches.SingleOrDefault(w => w.IsEnabled && (w.Query == builder.ToQuery()));
+                if (cache != null)
+                    return (T) cache.Result;
+                var result = await dispatcher.Invoke(query);
+                _queryCaches.Add(new QueryCache {Query = builder.ToQuery(), Result = result});
+                return result;
+            }
         }
 
         public void Clear()
