32ad9385595d2191c8d16afb0be8728b5bd99d5a
Mikazuki <mikazuki_fuyuno@outlook.com>
2017-04-04 03:20:39 +0900

    :sparkles: Convert HTML strings to UWP controls.

---
+++ Source/Pyxis/Behaviors/RichTextBlockBindingBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+
+using Microsoft.Xaml.Interactivity;
+
+namespace Pyxis.Behaviors
+{
+    internal class RichTextBlockBindingBehavior : Behavior<RichTextBlock>
+    {
+        public static readonly DependencyProperty BindingDocumentProperty =
+            DependencyProperty.Register(nameof(BindingDocument), typeof(List<Block>), typeof(RichTextBlockBindingBehavior),
+                                        new PropertyMetadata(null, PropertyChangedCallback));
+
+        public List<Block> BindingDocument
+        {
+            get { return (List<Block>) GetValue(BindingDocumentProperty); }
+            set { SetValue(BindingDocumentProperty, value); }
+        }
+
+        private static void PropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var textBlock = (sender as RichTextBlockBindingBehavior)?.AssociatedObject;
+            textBlock?.Blocks.Clear();
+
+            var blockCollection = e.NewValue as List<Block>;
+            if (blockCollection == null)
+                return;
+            foreach (var block in blockCollection)
+                textBlock?.Blocks.Add(block);
+        }
+    }
+}
