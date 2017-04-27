4e96eced573e4dd53dd3459269d5e84e75a8be0c
Mikazuki <mikazuki_fuyuno@outlook.com>
2017-04-27 10:05:57 +0900

    :bug: Fix for connection.

--- Source/Pyxis/ViewModels/BrowsingHistoryPageViewModel.cs
+++ Source/Pyxis/ViewModels/BrowsingHistoryPageViewModel.cs
@@ -17,7 +17,6 @@ namespace Pyxis.ViewModels
 {
     public class BrowsingHistoryPageViewModel : ViewModel
     {
-        // public ReactiveProperty<int> SelectedIndex { get; }
         public ReactiveProperty<object> SelectedItem { get; }
 
         public IncrementalLoadingCollection<PixivBrowsingHistorySource<Illust, IllustViewModel>, IllustViewModel> IllustSource { get; }
@@ -29,27 +28,6 @@ namespace Pyxis.ViewModels
                 new PixivBrowsingHistorySource<Illust, IllustViewModel>(pixivClient, w => new IllustViewModel(w)));
             NovelSource = new IncrementalLoadingCollection<PixivBrowsingHistorySource<Novel, NovelViewModel>, NovelViewModel>(
                 new PixivBrowsingHistorySource<Novel, NovelViewModel>(pixivClient, w => new NovelViewModel(w)));
-
-            /*
-            SelectedIndex = new ReactiveProperty<int>();
-            SelectedIndex.SelectMany(async w =>
-            {
-                switch (w)
-                {
-                    case 0:
-                        // await pixivRanking.FetchIllustRankingAsync(RankingMode.Day, null);
-                        break;
-
-                    case 1:
-                        // await pixivRanking.FetchMangaRankingAsync(RankingMode.DayManga, null);
-                        break;
-
-                    default:
-                        throw new NotSupportedException("Wrong Index Number of Pivot.");
-                }
-                return Unit.Default;
-            }).Subscribe().AddTo(this);
-            */
             SelectedItem = new ReactiveProperty<object>();
             SelectedItem.Select(w => w as ContentViewModel).Where(w => w != null)
                         .Subscribe(w => w.NavigateTo()).AddTo(this);
