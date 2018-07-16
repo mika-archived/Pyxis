606c2ddae3a6cdaeefded3faf694bab57033c4d3
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-07-17 00:08:51 +0900

    feat: Add behavior/converter for HTML strings.

---
+++ Source/Pyxis/Behaviors/RichTextBlockBindingBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+
+using Microsoft.Xaml.Interactivity;
+
+namespace Pyxis.Behaviors
+{
+    internal class RichTextBlockBindingBehavior : Behavior<RichTextBlock>
+    {
+        public static readonly DependencyProperty BindingDocumentProperty =
+            DependencyProperty.Register(nameof(BindingDocument), typeof(List<Block>), typeof(RichTextBlockBindingBehavior), new PropertyMetadata(null, PropertyChangedCallback));
+
+        public List<Block> BindingDocument
+        {
+            get => (List<Block>) GetValue(BindingDocumentProperty);
+            set => SetValue(BindingDocumentProperty, value);
+        }
+
+        private static void PropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var textBlock = (sender as RichTextBlockBindingBehavior)?.AssociatedObject;
+            textBlock?.Blocks.Clear();
+
+            if (!(e.NewValue is List<Block> blockCollection))
+                return;
+            foreach (var block in blockCollection)
+                textBlock?.Blocks.Add(block);
+        }
+    }
+}
---
+++ Source/Pyxis/Behaviors/UniformToParentControlBehavior.cs
@@ -0,0 +1,58 @@
+﻿using Windows.UI.Xaml;
+
+using Microsoft.Xaml.Interactivity;
+
+namespace Pyxis.Behaviors
+{
+    public class UniformToParentControlBehavior : Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty TargetProperty =
+            DependencyProperty.Register(nameof(Target), typeof(FrameworkElement), typeof(UniformToParentControlBehavior), new PropertyMetadata(null, PropertyChangedCallback));
+
+        public FrameworkElement Target
+        {
+            get => (FrameworkElement) GetValue(TargetProperty);
+            set => SetValue(TargetProperty, value);
+        }
+
+        private static void PropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ((FrameworkElement) e.NewValue).SizeChanged += ((UniformToParentControlBehavior) sender).ElementOnSizeChanged;
+        }
+
+        private void AssociatedObjectOnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            if (Target != null)
+                Target.SizeChanged += ElementOnSizeChanged;
+        }
+
+        private void ElementOnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Width ベース変換 (Desktop only)
+            if (AssociatedObject == null || double.IsNaN(AssociatedObject.MaxHeight))
+                return;
+            var target = AssociatedObject;
+            var aspectRatio = AssociatedObject.MaxHeight / AssociatedObject.MaxWidth;
+            var gridSize = e.NewSize;
+            target.Width = gridSize.Width;
+            target.Height = gridSize.Width * aspectRatio;
+        }
+
+        #region Overrides of Behavior<FrameworkElement>
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.DataContextChanged += AssociatedObjectOnDataContextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.DataContextChanged -= AssociatedObjectOnDataContextChanged;
+            Target.SizeChanged -= ElementOnSizeChanged;
+        }
+
+        #endregion
+    }
+}
