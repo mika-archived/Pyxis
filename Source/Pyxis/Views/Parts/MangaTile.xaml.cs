a1bc831575f2252395995fb319c7e42d464290a2
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-17 08:39:26 +0900

    refactor(parts): Remove unused references.

--- Source/Pyxis/Views/Parts/MangaTile.xaml.cs
+++ Source/Pyxis/Views/Parts/MangaTile.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
 
 // ユーザー コントロールの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234236 を参照してください
 
@@ -21,7 +8,7 @@ namespace Pyxis.Views.Parts
     {
         public MangaTile()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
